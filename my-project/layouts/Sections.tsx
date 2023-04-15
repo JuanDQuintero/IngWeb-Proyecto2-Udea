dcbc406aa9d446deb8eaef8a58a5a1fcccea9d15
AndersonSC <anderson.saldarriaga@udea.edu.com>
2023-04-15 18:42:57 -0500

    se añade la section 9

--- my-project/layouts/Sections.tsx
+++ my-project/layouts/Sections.tsx
@@ -1,6 +1,7 @@
 import Carrusel from '@components/Sections/Carrusel'
 import Novedades from '@components/Sections/Novedades'
 import Pagos from '@components/Sections/Pagos'
+import Sec9 from '@components/Sections/Sec9'
 import React from 'react'
 
 export default function Sections() {
@@ -10,8 +11,9 @@ export default function Sections() {
         <article>
             <Pagos />
             <Carrusel />
+            
         </article>
-        
+        <Sec9 />
     </div>
   )
 }
