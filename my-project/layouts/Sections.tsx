0de0ac9624527c613f03a6d1fc1c6e6e43c5fd63
Juan Diego Quintero <juan.quinterou@udea.edu.co>
2023-04-19 16:20:13 -0500

    cambios

--- my-project/layouts/Sections.tsx
+++ my-project/layouts/Sections.tsx
@@ -1,6 +1,7 @@
 import Carrusel from '@components/Sections/Carrusel'
 import Novedades from '@components/Sections/Novedades'
 import Pagos from '@components/Sections/Pagos'
+import Suscripcion from '@components/Sections/Suscripcion'
 import React from 'react'
 
 export default function Sections() {
@@ -10,6 +11,7 @@ export default function Sections() {
         <article>
             <Pagos />
             <Carrusel />
+            <Suscripcion />
         </article>
         
     </div>
