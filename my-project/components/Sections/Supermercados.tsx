c9873c87b91fd8ecadc11677cee9157d7bf16e85
Julian Castaño <julian.castanoj@udea.edu.co>
2023-04-23 17:10:58 -0500

    mitad del responsive

--- my-project/components/Sections/Supermercados.tsx
+++ my-project/components/Sections/Supermercados.tsx
@@ -6,23 +6,23 @@ export default function Sec7() {
         <section className="colecciones">
             <div className="title-section">
                 <h2>Colecciones: <span>Supermercado</span> <svg width="15" height="9" viewBox="0 0 15 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M13.7149 0.75L7.44338 7.0065L1.17188 0.75" stroke="#3483FA" strokeWidth="1.5" />
+                    <path d="M13.7149 0.75L7.44338 7.0065L1.17188 0.75" stroke="#3483FA" stroke-width="1.5" />
                 </svg>
                 </h2>
             </div>
             <div className="colecciones-list">
                 <div>
-                    <a href=""><img src="/supemercado.png" alt="supemercado"></img></a>
+                    <a href=""><img src="/supemercado.png" alt="supemercado" width="710px" height="710px"></img></a>
                 </div>
                 <div className="items-colecciones">
-                    <a href=""><img src="/perfume.png" alt="perfume" width="184px" height="184px"></img></a>
-                    <a href=""><img src="/jabon.png" alt="jabon" width="184px" height="184px"></img></a>
-                    <a href=""><img src="/nivea.png" alt="nivea" width="184px" height="184px"></img></a>
-                    <a href=""><img src="/cicatricure.png" alt="cicatricure" width="184px" height="184px"></img></a>
-                    <a href=""><img src="/nissin.png" alt="nissin" width="184px" height="184px"></img></a>
-                    <a href=""><img src="/licor.png" alt="licor" width="184px" height="184px"></img></a>
-                    <a href=""><img src="/crema.png" alt="crema" width="184px" height="184px"></img></a>
-                    <a href=""><img src="/shampoo.png" alt="shampoo" width="184px" height="184px"></img></a>
+                    <a href=""><img src="/perfume.png" alt="perfume" width="164px" height="164px"></img></a>
+                    <a href=""><img src="/jabon.png" alt="jabon" width="164px" height="164px"></img></a>
+                    <a href=""><img src="/nivea.png" alt="nivea" width="164px" height="164px"></img></a>
+                    <a href=""><img src="/cicatricure.png" alt="cicatricure" width="164px" height="164px"></img></a>
+                    <a href=""><img src="/nissin.png" alt="nissin" width="164px" height="164px"></img></a>
+                    <a href=""><img src="/licor.png" alt="licor" width="164px" height="164px"></img></a>
+                    <a href=""><img src="/crema.png" alt="crema" width="164px" height="164px"></img></a>
+                    <a href=""><img src="/shampoo.png" alt="shampoo" width="164px" height="164px"></img></a>
 
                 </div>
             </div>
